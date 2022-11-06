83e21a80e2124e53eac83dde5d7c970a7777eedf
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-11-06 16:31:36 +0100

    add version

--- index.mjs
+++ index.mjs
@@ -5,11 +5,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    const message = `Hello Kubernetes, I'am Pod ${os.hostname()}`;
+    const message = `Hello Kubernetes, I'am Pod ${os.hostname()}  Version V1.4.0`;
   res.send(message);
 })
 
 app.listen(PORT, () =>{
-    console.log(`web server listen at port ${PORT} Version V1.0.1`);
-    console.log(`web server ==  ${os.hostname()}`);
+    console.log(`web server listen at port ${PORT} Version V1.0.2`);
+    console.log(`web server ==  ${os.hostname()} Version V1.0.3`);
 })
